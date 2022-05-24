8844830fabb48d7fed808e93fea137a6be9511ff
mazeneldeeb2 <mazeneldeeb9@gmail.com>
2022-05-25 00:08:40 +0200

    hangman

--- main.c
+++ main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <conio.h>
 
 
 
 int main()
 {
-    char* words[] = {"dogs", "ziyadBs7nyaka",};			
-    char dots[20];
-    char* word;
-    int random;             // random number to choose word from set of words			
+    char word[20];			//Used to store the user word. Most words are less than 20 characters so the default size is 20.
+    char dots[20];			//A string full of * that are replaced upon the user entering the right letter of the word.
+    int counter = 0;			//A counter to use in the for loops.
     int len;				//len is the length of the word entered.
     char ch;				//ch is a temporary character variable.
     int strikes=0;			//strikes counts how many wrong characters the user has guessed.
@@ -16,16 +16,15 @@ int main()
     int wincounter=0;			//wincounter keeps track of the number of correct guesses.
     int i;				//Another counter variable to be used in for loops.
 
-    printf("Enter random number between 1 and 10 to choose random word from set of words\n");
-    scanf("%d", &random);
-    word = words[random];
-
    
+
+    printf("\n\nEnter a word:");			//Ask the user to enter a word.
+    scanf("%s", &word);
     len = strlen(word);					//Make len equal to the length of the word.
 
 
     //Fill the stars string with * according to the input word length (len)
-    for(int counter=0; counter<len; counter++)
+    for(counter=0; counter<len; counter++)
     {
         dots[counter]='.';
     }
@@ -34,28 +33,29 @@ int main()
     dots[len]='\0';					//Insert the terminating string character at the end of the stars string.
 
 
-    //Enter main program loop where guessing and checking happens. this condition is for length of the word  + 10 wrong guess.
-    for(int counter = 0; counter< len + 10; counter++)
+    //Enter main program loop where guessing and checking happens. this condition is for length of the word + 10 strike characters.
+    for(counter = 0; counter<len + 10; counter++)
     {
 
         if(wincounter==len)			    	//If the number of correct guesses matches the length of the word it means that the user won.
         {
-            printf("The word was: %s\n", word);
+            printf("\n\nThe word was: %s\n", word);
             printf("\nYou win!\n");
             break;
         }
 
+        
 
-        if(strikes== 10 )				    	//If the user makes 10 wrong guesses it means that he lost.
+        if(strikes== 10)				    	//If the user makes 10 wrong guesses it means that he lost.
         {
             printf("\n\nThe word was: %s\n", word);
             printf("\n\nYou lose.\n");
             break;
         }
 
-        printf("%s", dots);			//Print the stars string (i.e: h*ll* for hello).
+        printf("\n\n\n\n%s", dots);			//Print the stars string (i.e: h*ll* for hello).
 
-	printf("Guess a letter:");			//Have the user guess a letter.
+	    printf("\n\nGuess a letter:");			//Have the user guess a letter.
         scanf(" %c",&ch);
 
         for(i=0; i<len; i++)            		//Run through the string checking the characters.
@@ -76,5 +76,6 @@ int main()
         trigger = 0;				    	//Set the trigger to 0 again so the user can guess a new character.
     }
 
+   
    
 }
